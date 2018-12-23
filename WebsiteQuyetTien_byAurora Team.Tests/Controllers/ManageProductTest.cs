e5f289f719afdf2836e6eeab5f2c2b11e462394d
Pham The Vi <vpham3218@gmail.com>
2018-12-23 22:47:57 +0700

    Cau Hinh Test

---
+++ WebsiteQuyetTien_byAurora Team.Tests/Controllers/ManageProductTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebsiteQuyetTien_byAurora_Team.Tests.Controllers
+{
+    /// <summary>
+    /// Summary description for ManageProductTest
+    /// </summary>
+    [TestClass]
+    public class ManageProductTest
+    {
+        public ManageProductTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+        // Test Add Product
+        [TestMethod]
+        public void TestAddProduct()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+
+        //Test Edit Product
+        [TestMethod]
+        public void TestEditProduct()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+
+        //Test Delete Product
+        [TestMethod]
+        public void TestDeleteProduct()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+
+        //Test Search Product
+        [TestMethod]
+        public void TestSearchProduct()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
