c0de1326cba2d85aa22ddebda419c865b9efdc8f
HuyTrieu <tommy.jimmy2712@gmail.com>
2018-12-25 21:46:40 +0700

    fix sth

--- WebsiteQuyetTien_byAurora Team/Controllers/HomeController.cs
+++ WebsiteQuyetTien_byAurora Team/Controllers/HomeController.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebsiteQuyetTien_byAurora_Team.Models;
 
 namespace WebsiteQuyetTien_byAurora_Team.Controllers
 {
     public class HomeController : Controller
     {
+        DmQT08Entities db = new DmQT08Entities();
         //Giao diện chính
         public ActionResult Index()
         {
-            return View();
+
+            //Lây ra danh sách 8 sản phẩm bán chạy nhất
+            var lstProBestSale = db.CashBillDetails.OrderByDescending(n => n.Quantity).GroupBy(n => n.ProductID);
+            ViewBag.lstProBestSale = lstProBestSale;
+            //Lấy danh sách 8 sản phẩm mới nhất
+            var lstProNew = db.Products.Where(n=>n.Status==true).OrderByDescending(n=>n.ID);
+            return View(lstProNew);
         }
         //Giao diện về chúng tôi
         public ActionResult About()
@@ -33,6 +42,24 @@ namespace WebsiteQuyetTien_byAurora_Team.Controllers
         public ActionResult News()
         {
             return View();
+        }
+        //Giao diện tin tức sự kiện chi tiết
+        public ActionResult NewsDetail()
+        {
+            return View();
+        }
+        //Giao diện không tìm thấy trang
+        public ActionResult Error()
+        {
+            return View();
+        }
+
+
+        //Menu droplist danh sách sản phẩm hiển thị theo loại sản phẩm và nhà cung cấp
+        public ActionResult MenuPartial()
+        {
+            var listPro = db.Products.Where(n=>n.Status==true);
+            return PartialView(listPro);
         }
     }
 }
---
+++ WebsiteQuyetTien_byAurora Team/Controllers/ProductController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteQuyetTien_byAurora_Team.Models;
+
+namespace WebsiteQuyetTien_byAurora_Team.Controllers
+{
+    public class ProductController : Controller
+    {
+        public DmQT08Entities db = new DmQT08Entities();
+
+        //Xem danh sách sản phẩm theo loại sản phẩm
+        public ActionResult ViewListProductWithType(int? ProductTypeID)
+        {
+            //lấy ra danh sách các sản phẩm theo loại sản phẩm được yêu cầu
+            var lstProductOfType = db.Products.Where(n => n.ProductTypeID == ProductTypeID && n.Status == true);
+            var productType = db.ProductTypes.SingleOrDefault(n => n.ID == ProductTypeID);
+            //Kiểm tra có tồn tại loại sản phẩm đó không
+            if (productType != null)
+            {
+                ViewBag.ProductType = productType;
+                return View(lstProductOfType);
+            }
+            return RedirectToAction("Error", "Home");
+        }
+
+        //Xem danh sách sản phẩm đươc xếp theo nhà sản xuất
+        public ActionResult ViewListProductWithManufactor(int? ProductTypeID, int? ManufactoryID)
+        {
+            //lấy ra danh sách các sản phẩm theo loại sản phẩm được yêu cầu và theo đúng nhà sản xuất
+            var lstProductOfType = db.Products.Where(n => n.ProductTypeID == ProductTypeID && n.Status == true && n.ManufactoryID == ManufactoryID);
+            var productType = db.ProductTypes.SingleOrDefault(n => n.ID == ProductTypeID);
+            var productManu = db.Manufactories.SingleOrDefault(n => n.ID == ManufactoryID);
+            
+            //Kiểm tra có tồn tại nhà sản xuất đó không
+            if (productType != null && productManu != null)
+            {
+                ViewBag.ProductType = productType;
+                ViewBag.productManu = productManu;
+                return View(lstProductOfType);
+            }
+            return RedirectToAction("Error", "Home");
+        }
+
+        //Trang xem chi tiết sản phẩm
+        public ActionResult ViewDetail(int? ID)
+        {
+            var pro = db.Products.SingleOrDefault(n => n.ID == ID && n.Status == true);
+            if(pro != null)
+            {
+                return View(pro);
+            }
+            return RedirectToAction("Error", "Home");
+        }
+
+        //Cột thể loại sản phẩm Partial
+        public ActionResult CategoryPartial()
+        {
+            var lstProduct = db.Products.Where(n=>n.Status==true);
+            return PartialView(lstProduct);
+        }
+        //Hiển thị top sản phẩm bán chạy nhất
+        public ActionResult ShowTopSale()
+        {
+            var sale = db.CashBillDetails.Where(n=>n.Product.Status==true).OrderByDescending(n=>n.Quantity);
+            return PartialView(sale);
+        }
+    }
+}
