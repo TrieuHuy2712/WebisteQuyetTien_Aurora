d4b4b6db7df12fb860aeaf2f940ea3fb4bc33d60
Pham The Vi <vpham3218@gmail.com>
2018-12-29 20:17:52 +0700

    Test Product Type

--- WebsiteQuyetTien_byAurora Team.Tests/Controllers/ProductTypeControllerTest.cs
+++ WebsiteQuyetTien_byAurora Team.Tests/Controllers/ProductTypeControllerTest.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.Collections;
 using System.Linq;
+using System.Transactions;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using WebsiteQuyetTien_byAurora_Team.Controllers;
 using WebsiteQuyetTien_byAurora_Team.Models;
-
 namespace WebsiteQuyetTien_byAurora_Team.Tests.Controllers
 {
     [TestClass]
@@ -38,6 +38,50 @@ namespace WebsiteQuyetTien_byAurora_Team.Tests.Controllers
                 model.ProductTypeCode,
                 model.ProductTypeName,
             }), JsonConvert.SerializeObject(result.Data));
+        }
+        [TestMethod]
+        public void TestSaveEntity1()
+        {
+            var db = new DmQT08Entities();
+            var model = new ProductType();
+
+            model.ProductTypeCode = "CHE";
+            model.ProductTypeName = "CHelsea";
+
+            int count = db.ProductTypes.Count();
+            var controller = new ProductTypeController();
+            using (var scope = new TransactionScope())
+            {
+                model.ID = 0;
+                var result0 = controller.SaveEntity(model) as JsonResult;
+                Assert.IsNotNull(result0);
+                Assert.AreEqual(count + 1, db.ProductTypes.Count());
+
+                Assert.IsInstanceOfType(result0.Data, typeof(ProductType));
+                var productType = result0.Data as ProductType;
+                Assert.AreEqual("CHelsea", productType.ProductTypeName);
+            }
+        }
+        [TestMethod]
+        public void TestSaveEntity2()
+        {
+            var db = new DmQT08Entities();
+            var model = db.ProductTypes.AsNoTracking().First();
+
+            int count = db.ProductTypes.Count();
+            var controller = new ProductTypeController();
+            using (var scope = new TransactionScope())
+            {
+                model.ID = 1;
+                model.ProductTypeName = "Hazard";
+                var result0 = controller.SaveEntity(model) as JsonResult;
+                Assert.IsNotNull(result0);
+                Assert.AreEqual(count, db.ProductTypes.Count());
+
+                Assert.IsInstanceOfType(result0.Data, typeof(ProductType));
+                var productType = result0.Data as ProductType;
+                Assert.AreEqual("Hazard", productType.ProductTypeName);
+            }
         }
     }
 }
